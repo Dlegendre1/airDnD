93f0e496a1e97fdac5100162ab3f81e91358bffb
Daniel <daniellegendre1221@gmail.com>
2023-09-16 19:06:20 -0400

    finish generic error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
-
+const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -41,6 +41,15 @@ app.use(
 app.use(routes);
 
 
+
+
+
+
+
+
+
+/*                   ERROR HANDLING MIDDLEWARE               */
+
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
@@ -49,17 +58,23 @@ app.use((_req, _res, next) => {
     next(err);
 });
 
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.use((err, _req, res, _next) => {
+    res.status(err.status) || 500;
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
