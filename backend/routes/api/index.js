d55adec62b6a6c185426cb9d74ea6d9d0ff725f8
Daniel <daniellegendre1221@gmail.com>
2023-09-16 16:33:53 -0400

    add api router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
+
+
+
+"UZsUKCvT-m7oU_q-lnkqMrTxFOeGyYXrzRcc"
+
+
+
+
+
+
+
+
+
+module.exports = router;
