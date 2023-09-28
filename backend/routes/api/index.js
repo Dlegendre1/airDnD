ade4f56e02b2fbddd51c5a1ad57774b97f2abb2a
Daniel <daniellegendre1221@gmail.com>
2023-09-28 18:41:23 -0400

    finish database and tested seeders

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session');
 const usersRouter = require('./users');
+const spotsRouter = require('./spots');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -8,6 +9,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+// router.use('/spots', spotsRouter);
 
 
 router.post('/test', function (req, res) {
@@ -21,4 +24,3 @@ router.post('/test', function (req, res) {
 
 
 module.exports = router;
-
