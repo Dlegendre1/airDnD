33deb9487b6a84a8543f6ed14b4bae5b710d63ed
Daniel <daniellegendre1221@gmail.com>
2023-10-02 18:48:11 -0400

    partial image to spot route, add current user spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot } = require('../../db/models');
+const { User, Spot, SpotImage } = require('../../db/models');
 
 const router = express.Router();
 
-
+//Get all spots
 router.get(
     '/',
     async (req, res, next) => {
@@ -36,6 +36,36 @@ router.get(
     }
 );
 
+//Get spots of current user
+router.get(
+    '/current',
+    async (req, res, next) => {
+        const userId = req.user.id;
+        const userSpots = await Spot.findAll({ where: { ownerId: userId } });
+
+        const spots = userSpots.map(spot => ({
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            avgRating: spot.avgRating,
+            previewImage: spot.previewImage
+        }));
+
+        res.json({ Spots: spots });
+    }
+);
+
+//Post a new spot
 router.post(
     '/',
     async (req, res, next) => {
@@ -55,14 +85,23 @@ router.post(
             description: spot.description,
             price: spot.price
         };
-
+        await setTokenCookie(res, req.user);
         return res.json({
             spot: safeSpot
         });
     }
 );
 
+// //Add image to spot based on spot id
+// router.post(
+//     '/:spotId/images', requireAuth,
+//     async (req, res, next) => {
+//         const { url, preview } = req.body;
+//         const spotImage = await SpotImage.create({ url, preview });
+//         //STILL HAVE WORK TO DO
 
+//     }
+// );
 
 
 
