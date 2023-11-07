172034ebb223ddc183328db1ede82b8617e83a09
Daniel <daniellegendre1221@gmail.com>
2023-11-06 19:32:13 -0500

    frontend authme profile button fix

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
+import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import OpenModalButton from "../OpenModalButton";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef();
+
 
     const openMenu = () => {
         if (showMenu) return;
@@ -19,8 +21,8 @@ function ProfileButton({ user }) {
         if (!showMenu) return;
 
         const closeMenu = (e) => {
-            if (!ulRef.current.contains(e.target)) {
-                setShowMenu(false);
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                return setShowMenu(false);
             }
         };
 
@@ -44,35 +46,37 @@ function ProfileButton({ user }) {
             <button onClick={openMenu}>
                 <i className="fas fa-user-circle" />
             </button>
-            <ul className={ulClassName} ref={ulRef}>
-                {user ? (
-                    <>
-                        <li>{user.username}</li>
-                        <li>{user.firstName} {user.lastName}</li>
-                        <li>{user.email}</li>
-                        <li>
-                            <button onClick={logout}>Log Out</button>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <li>
-                            <OpenModalButton
-                                buttonText="Log In"
-                                onButtonClick={closeMenu}
-                                modalComponent={<LoginFormModal />}
-                            />
-                        </li>
-                        <li>
-                            <OpenModalButton
-                                buttonText="Sign Up"
-                                onButtonClick={closeMenu}
-                                modalComponent={<SignupFormModal />}
-                            />
-                        </li>
-                    </>
-                )}
-            </ul>
+            {showMenu && (
+                <ul className={ulClassName} ref={ulRef}>
+                    {user ? (
+                        <>
+                            <li>{user.username}</li>
+                            <li>{user.firstName} {user.lastName}</li>
+                            <li>{user.email}</li>
+                            <li>
+                                <button onClick={logout}>Log Out</button>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <OpenModalButton
+                                    buttonText="Log In"
+                                    onButtonClick={closeMenu}
+                                    modalComponent={<LoginFormModal />}
+                                />
+                            </li>
+                            <li>
+                                <OpenModalButton
+                                    buttonText="Sign Up"
+                                    onButtonClick={closeMenu}
+                                    modalComponent={<SignupFormModal />}
+                                />
+                            </li>
+                        </>
+                    )}
+                </ul>
+            )}
         </>
     );
 }
